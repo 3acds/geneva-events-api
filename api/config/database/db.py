51af144ef401e36f449fe9650c79120464895ba9
3run0Silva <dasilva.92.b@gmail.com>
2026-07-12 16:03:12 +0200

    Load Firebase credentials from environment

--- api/config/database/db.py
+++ api/config/database/db.py
@@ -1,10 +1,8 @@
-import os
 import firebase_admin
-from firebase_admin import firestore, credentials
+from firebase_admin import firestore
 
-cred_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cred.json')
-cred = credentials.Certificate(cred_path)
-firebase_admin.initialize_app(cred)
+firebase_admin.initialize_app()
+
 
 def get_db():
     return firestore.client()
